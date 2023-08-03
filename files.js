5af0013b12478ba9d2093854a8f0cd2d41cc88c2
tom-avilius <aaarshdwivedi244475@gmail.com>
2023-08-03 15:58:51 +0530

    Add function to create an empty file.

--- files.js
+++ files.js
@@ -105,7 +105,7 @@ export default class ManageInternalFiles {
             }
         })
 
-        ManageInternalFiles.appendData('$generalConfig.json', "names", fileName);
+        ManageInternalFiles.appendData('$generalConfig.json', "names", fileName+'');
     }
 
 
